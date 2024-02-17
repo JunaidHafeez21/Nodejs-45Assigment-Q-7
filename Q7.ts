91d69b89687d7675bc0e78481ed33aec75ed2ef7
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:30:40 +0500

    Add files via upload

---
+++ Q7.ts
@@ -0,0 +1,8 @@
+// Q7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let a: number = 20
+let b: number = 10
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
